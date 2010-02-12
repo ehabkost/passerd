b4b458c01b43fca8be96c6802cd70e29df4e1e50
Eduardo Habkost <ehabkost@raisama.net>
2010-02-11 23:10:15 -0200

    dialogs.py: copyright info
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- passerd/dialogs.py
+++ passerd/dialogs.py
@@ -1,3 +1,28 @@
+# Passerd - An IRC server as a gateway to Twitter
+#
+# classes for text-based "dialogs"
+#
+# Author: Eduardo Habkost <ehabkost@raisama.net>
+#
+# Copyright (c) 2009,2010 Eduardo Pereira Habkost <ehabkost@raisama.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 import re
 
 class Dialog:
